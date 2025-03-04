15e418c8dcd3bc97ae3d8c92773fecfa3a912899
Hainkinho <41513606+Hainkinho@users.noreply.github.com>
2025-03-04 08:32:18 +0100

    Initial Commit

---
+++ Imaginary/ImaginaryApp.swift
@@ -0,0 +1,19 @@
+//
+//
+//  ImaginaryApp.swift
+//  Imaginary
+//
+//  Created by Haipp on 04.03.25.
+//  
+	
+
+import SwiftUI
+
+@main
+struct ImaginaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
