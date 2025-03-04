0f40740a71f811508dd6b2d7e6a95359dd3dbb70
Hainkinho <41513606+Hainkinho@users.noreply.github.com>
2025-03-04 10:04:29 +0100

    Add basic DetailsPage version with dummy data

---
+++ Imaginary/UI/DetailsPage.swift
@@ -0,0 +1,77 @@
+//
+//
+//  DetailsPage.swift
+//  Imaginary
+//
+//  Created by Haipp on 04.03.25.
+//  
+	
+
+import SwiftUI
+
+struct DetailsPage: View {
+	
+	let showcaseImageURL: URL
+	let title: String
+	let description: String
+	let isBookable: Bool
+	let startDate: Date
+	let endDate: Date
+	
+	var body: some View {
+		ScrollView {
+			VStack(spacing: 0) {
+				Rectangle()
+					.frame(maxWidth: .infinity)
+					.frame(height: 300)
+				
+				VStack(alignment: .leading, spacing: 8) {
+					Text(title)
+						.fontWeight(.bold)
+					
+					Text(description)
+						.frame(minHeight: 150, alignment: .top)
+					
+					Text(isBookable ? "Bookable" : "Not Bookable")
+						.fontWeight(.bold)
+					
+					Text("Start - End")
+				}
+				.frame(maxWidth: .infinity, alignment: .leading)
+				.padding(.vertical, 40)
+				
+				Button(action: {}) {
+					Text("CALL TO BOOK")
+						.foregroundStyle(.white)
+						.fontWeight(.bold)
+						.font(.title3)
+						.frame(maxWidth: .infinity)
+						.padding(.horizontal, 50) // Prevents image from overlapping text if it becomes too long
+						.overlay(alignment: .leading) {
+							Image(Constants.companyLogoImageString)
+								.resizable()
+								.frame(width: 40, height: 40)
+								.padding(.leading)
+						}
+						.frame(maxWidth: .infinity)
+						.frame(minHeight: 65)
+						.background(Color.blue)
+				}
+			}
+			.padding(.horizontal)
+			.padding(.vertical)
+		}
+	}
+}
+
+
+#Preview {
+	DetailsPage(
+		showcaseImageURL: URL.placeholderImageURL,
+		title: "Title",
+		description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
+		isBookable: true,
+		startDate: .now.addingTimeInterval(-1000),
+		endDate: .now
+	)
+}
